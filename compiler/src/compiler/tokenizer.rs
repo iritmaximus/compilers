78f170ed7d028cf5a9348ab007733172d910363f
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-01-15 17:00:09 +0200

    added tests to tokenizer

--- compiler/src/compiler/tokenizer.rs
+++ compiler/src/compiler/tokenizer.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-pub fn tokenizer(code: &str) {
+pub fn tokenizer(code: &str) -> Vec<&str> {
     // Damn, regex...
     println!("Doing regex things for {}", code);
     let re = Regex::new(
@@ -23,7 +23,7 @@ pub fn tokenizer(code: &str) {
         })
         .collect();
     println!("{tokens:?}");
-    // return tokens;
+    return tokens;
 }
 
 #[cfg(test)]
@@ -33,17 +33,44 @@ mod tests {
     #[test]
     fn single_integer_literals() {
         let code = "143";
-        let result = tokenizer(code);
-
-        // assert_eq!(result[0], "143");
+        assert_eq!(tokenizer(code), ["143"]);
     }
-
     #[test]
     fn multiple_integer_literals() {
         let code = "532 125390";
-        let result = tokenizer(code);
+        assert_eq!(tokenizer(code), ["532", "125390"]);
+    }
 
-        // assert_eq!(result[0], "532");
-        // assert_eq!(result[1], "125390");
+    #[test]
+    fn comment_hashtag() {
+        let code = "# This is a comment :)";
+        let compare: Vec<&str> = vec![code];
+        assert_eq!(tokenizer(code), compare);
+    }
+    #[test]
+    fn comment_div() {
+        let code = "// This is a comment :)";
+        let compare: Vec<&str> = vec![code];
+        assert_eq!(tokenizer(code), compare);
+    }
+
+    #[test]
+    fn math_operators() {
+        let code = "1 + 2 * 5";
+        let compare: Vec<&str> = vec!["1", "+", "2", "*", "5"];
+        assert_eq!(tokenizer(code), compare);
+    }
+    #[test]
+    fn identifiers_with_whitespace() {
+        let code = "if  3\nwhile";
+        let compare: Vec<&str> = vec!["if", "3", "while"];
+        assert_eq!(tokenizer(code), compare);
+    }
+
+    #[test]
+    fn more_complex_comxinations() {
+        let code = "if a <= bee then print_int(123)";
+        let compare: Vec<&str> = vec!["if", "a", "<=", "bee", "then", "print_int", "(", "123", ")"];
+        assert_eq!(tokenizer(code), compare);
     }
 }
