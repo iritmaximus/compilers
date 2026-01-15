aad5d225efd3974af51e33bb685700bfc9696ed2
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-01-15 13:08:34 +0200

    more working rust baseline

---
+++ compiler/src/compiler/mod.rs
@@ -0,0 +1,4 @@
+pub fn compile(code: &str) -> &str {
+    println!("Code: {}", &code);
+    return code;
+}
--- compiler/src/main.rs
+++ compiler/src/main.rs
@@ -4,6 +4,8 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+pub mod compiler;
+
 const PORT: &str = "3000";
 const HOST: &str = "0.0.0.0";
 
@@ -20,37 +22,43 @@ fn main() {
     }
 }
 
-fn compile(code: &str) -> &str {
-    println!("Code: {}", &code);
-    return code;
-}
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader.lines().map(|result| result.unwrap()).collect();
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
     let body = http_request.last().unwrap();
+
+    if body.is_empty() {
+        eprintln!("Empty body!");
+        let response = "HTTP/1.1 404 NOT FOUND";
+        stream.write_all(response.as_bytes()).unwrap();
+        return;
+    }
 
     let json = json::parse(body).unwrap();
     let command = json["command"].as_str().unwrap();
 
     println!("Request: {http_request:#?}");
-    println!("Body: {json:#?}");
+    // println!("Body: {json:#?}");
     println!("Command: {command:#?}");
 
     match command {
         "ping" => {
             println!("Pong!");
-            let response = "HTTP/1.1 200 OK";
+            let response = "{}";
             stream.write_all(response.as_bytes()).unwrap();
         }
         "compile" => {
             let code = json["code"].as_str().unwrap();
-            let executable = compile(code);
+            let executable = compiler::compile(code);
             let content = BASE64_STANDARD.encode(executable);
-            let length = content.len();
 
-            let response = format!("HTTP/1.1 200 OK\rContent-Length: {length}\r\n\r{content}");
+            let response = format!("{{\"program\": \"{}\"}}", content.as_str());
+            println!("Response: {:?}", response);
             stream.write_all(response.as_bytes()).unwrap();
         }
         _ => {
