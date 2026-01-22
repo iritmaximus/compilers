6b2bfb8915e366f694b573f531e5f35353308ff4
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-01-22 10:13:53 +0200

    todo comments

--- compiler/src/compiler/parser.rs
+++ compiler/src/compiler/parser.rs
@@ -1,3 +1,10 @@
 use crate::compiler::tokenizer::Token;
 
+/*
+* 1. Create structs for all token types
+* 2. Make parse-function for ecah token types
+* 3. General parse function
+* 4. ???
+* 5. Profit
+*/
 pub fn parser(tokens: Vec<Token>) {}
