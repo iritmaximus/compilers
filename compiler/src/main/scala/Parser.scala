0819bdd926d0a18fe64b4b1b0ea0dd256c5ec8e9
Aaro Koivukangas <aaro.koivukangas@gmail.com>
2026-02-02 12:34:24 +0200

    template for parser

--- compiler/src/main/scala/Parser.scala
+++ compiler/src/main/scala/Parser.scala
@@ -21,12 +21,25 @@ class Parser(tokens: List[Token]):
   def peek(): Token =
     return tokens(pos)
 
-  def consume(expected: TokenType | List[TokenType]): Token =
+  def consume(expected: Option[TokenType | List[TokenType]] = None): Token =
     val token = peek()
+
+    expected match {
+      case Some(types) => types match {
+        case that: TokenType => println("IMPLEMENT TOKENTYPE")
+        case that: List[TokenType] => println("IMPLEMENT LIST[TOKENTYPE]")
+      }
+      case None => println("IMPLEMENT NONE")
+    }
     
     pos += 1
     return tokens(pos)
 
+  def handleExpected(token: Token, expected: TokenType): Try[Token] =
+    return Success(token)
+
 
 object Parser:
-  def parse(tokens: List[Token]): Try[Expression] = return Success(Other())
+  def parse(tokens: List[Token]): Try[Expression] =
+    val parser = Parser(tokens)
+    return Success(Other())
